3b9c5bf40b7a7c645fbb048a7d9ebf419f4e8ab8
Le Lag <gm.lelag@gmail.com>
2011-03-18 17:50:58 +0100

    initial commit

---
+++ tests/TestTest.php
@@ -0,0 +1,8 @@
+<?php
+require_once dirname(__FILE__).'/../lib/otphp.php';
+
+class TestTest extends PHPUnit_Framework_TestCase {
+  public function testThatPHPUnitWorks() {
+    $this->assertEquals(1,1);
+  }
+}
---
+++ vendor/libs.php
@@ -0,0 +1,4 @@
+<?php
+// Add any needed third party library to this directory
+
+//require_once dirname(__FILE__).'/some_lib/lib.php';
