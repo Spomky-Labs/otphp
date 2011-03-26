87e053b6ff0fa2f967aa1d1af5a3e9c338bb3512
Le Lag <gm.lelag@gmail.com>
2011-03-26 19:25:41 +0100

    Adding comments

--- lib/hotp.php
+++ lib/hotp.php
@@ -1,15 +1,50 @@
 <?php
 namespace OTPHP {
-
+  /**
+   * HOTP - One time password generator 
+   * 
+   * The HOTP class allow for the generation 
+   * and verification of one-time password using 
+   * the HOTP specified algorithm.
+   *
+   * This class is meant to be compatible with 
+   * Google Authenticator
+   *
+   * This class was originally ported from the rotp
+   * ruby library available at https://github.com/mdp/rotp
+   */
   class HOTP extends OTP {
+    /**
+     *  Get the password for a specific counter value
+     *  @param integer $count the counter which is used to
+     *  seed the hmac hash function.
+     *  @return integer the One Time Password
+     */
     public function at($count) {
       return $this->generateOTP($count);
     }
 
+
+    /**
+     * Verify if a password is valid for a specific counter value
+     *
+     * @param integer $otp the one-time password 
+     * @param integer $counter the counter value
+     * @return  bool true if the counter is valid, false otherwise
+     */
     public function verify($otp, $counter) {
       return ($otp == $this->at($counter));
     }
 
+    /**
+     * Returns the uri for a specific secret for hotp method.
+     * Can be encoded as a image for simple configuration in 
+     * Google Authenticator.
+     *
+     * @param string $name the name of the account / profile
+     * @param integer $initial_count the initial counter 
+     * @return string the uri for the hmac secret
+     */
     public function provisioning_uri($name, $initial_count) {
       return "otpauth://hotp/".urlencode($name)."?secret={$this->secret}&counter=$initial_count";
     }
--- lib/totp.php
+++ lib/totp.php
@@ -1,6 +1,24 @@
 <?php
 namespace OTPHP {
+  /**
+   * TOTP - One time password generator 
+   * 
+   * The TOTP class allow for the generation 
+   * and verification of one-time password using 
+   * the TOTP specified algorithm.
+   *
+   * This class is meant to be compatible with 
+   * Google Authenticator
+   *
+   * This class was originally ported from the rotp
+   * ruby library available at https://github.com/mdp/rotp
+   */
   class TOTP extends OTP {
+    /**
+     * The interval in seconds for a one-time password timeframe
+     * Defaults to 30
+     * @var integer
+     */
     public $interval;
 
     public function __construct($s, $opt = Array()) {
@@ -8,24 +26,57 @@ namespace OTPHP {
       parent::__construct($s, $opt);
     }
 
+    /**
+     *  Get the password for a specific timestamp value 
+     *
+     *  @param integer $timestamp the timestamp which is timecoded and 
+     *  used to seed the hmac hash function.
+     *  @return integer the One Time Password
+     */
     public function at($timestamp) {
       return $this->generateOTP($this->timecode($timestamp));
     }
 
+    /**
+     *  Get the password for the current timestamp value 
+     *
+     *  @return integer the current One Time Password
+     */
     public function now() {
       return $this->generateOTP($this->timecode(time()));
     }
 
+    /**
+     * Verify if a password is valid for a specific counter value
+     *
+     * @param integer $otp the one-time password 
+     * @param integer $timestamp the timestamp for the a given time, defaults to current time.
+     * @return  bool true if the counter is valid, false otherwise
+     */
     public function verify($otp, $timestamp = null) {
       if($timestamp === null)
         $timestamp = time();
       return ($otp == $this->at($timestamp));
     }
 
+    /**
+     * Returns the uri for a specific secret for totp method.
+     * Can be encoded as a image for simple configuration in 
+     * Google Authenticator.
+     *
+     * @param string $name the name of the account / profile
+     * @return string the uri for the hmac secret
+     */
     public function provisioning_uri($name) {
       return "otpauth://totp/".urlencode($name)."?secret={$this->secret}";
     }
 
+    /**
+     * Transform a timestamp in a counter based on specified internal
+     *
+     * @param integer $timestamp
+     * @return integer the timecode
+     */
     protected function timecode($timestamp) {
       return (int)( (((int)$timestamp * 1000) / ($this->interval * 1000)));
     }
