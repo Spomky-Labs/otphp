f311ef0fcef245b25871b33b6fe289efc512f833
Christian Scheb <cscheb@infernum.com>
2014-01-10 16:20:10 +0100

    Added command for generating Google Authenticator secrets

---
+++ Command/GoogleSecretCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace Scheb\TwoFactorBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GoogleSecretCommand extends ContainerAwareCommand
+{
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Command\Command::configure()
+     */
+    public function configure()
+    {
+        $this
+            ->setName("scheb:two-factor:google-secret")
+            ->setDescription('Generate a secret for Google Authenticator.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->getContainer()->has("scheb_two_factor.security.google_authenticator")) {
+            throw new \RuntimeException("Google Authenticator two-factor authentication is not enabled.");
+        }
+
+        $googleAuthenticator = $this->getContainer()->get("scheb_two_factor.security.google_authenticator");
+        $secret = $googleAuthenticator->generateSecret();
+
+        $output->writeln("<info>Secret:</info> ".$secret);
+    }
+}
