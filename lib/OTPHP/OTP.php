ee01b7d63d193381a89ff50f3e931888e5c6325b
Spomky <spomky+git@gmail.com>
2014-02-24 22:07:18 +0100

    OTP created

---
+++ lib/OTPHP/OTP.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace OTPHP;
+
+use Base32\Base32;
+
+abstract class OTP
+{
+    protected $secret;
+
+    protected $digest;
+
+    protected $digits;
+
+    public function __construct($secret, $digest = 'sha1', $digits = 6)
+    {
+
+        $this->setSecret($secret);
+        $this->setDigits($digits);
+        $this->setDigest($digest);
+    }
+
+    protected function generateOTP($input)
+    {
+        $hash = hash_hmac($this->getDigest(), $this->intToBytestring($input), $this->byteSecret());
+        $hmac = array();
+        foreach(str_split($hash, 2) as $hex) { // stupid PHP has bin2hex but no hex2bin WTF
+            $hmac[] = hexdec($hex);
+        }
+        $offset = $hmac[19] & 0xf;
+        $code = ($hmac[$offset+0] & 0x7F) << 24 |
+            ($hmac[$offset + 1] & 0xFF) << 16 |
+            ($hmac[$offset + 2] & 0xFF) << 8 |
+            ($hmac[$offset + 3] & 0xFF);
+        return $code % pow(10, $this->getDigits());
+    }
+
+    private function byteSecret()
+    {
+        return Base32::decode($this->getSecret());
+    }
+
+    private function intToBytestring($int)
+    {
+        $result = array();
+        while($int != 0) {
+            $result[] = chr($int & 0xFF);
+            $int >>= 8;
+        }
+        return str_pad(implode(array_reverse($result)), 8, "\000", STR_PAD_LEFT);
+    }
+
+    public function at($counter)
+    {
+        return $this->generateOTP($counter);
+    }
+
+
+    public function verify($otp, $counter)
+    {
+        return ($otp == $this->at($counter));
+    }
+
+    public function setSecret($secret)
+    {
+        $this->secret = $secret;
+        return $this;
+    }
+
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+
+    public function setDigits($digits)
+    {
+        $this->digits = $digits;
+        return $this;
+    }
+
+    public function getDigits()
+    {
+        return $this->digits;
+    }
+
+    public function setDigest($digest)
+    {
+        if( !in_array($digest, hash_algos()) ) {
+            throw new \Exception("'$digest' digest is not supported.");
+        }
+        $this->digest = $digest;
+        return $this;
+    }
+
+    public function getDigest()
+    {
+        return $this->digest;
+    }
+}
--- lib/OTPHP/TOTP.php
+++ lib/OTPHP/TOTP.php
@@ -1,30 +1,12 @@
 <?php
 
 namespace OTPHP;
-/**
- * TOTP - One time password generator 
- * 
- * The TOTP class allow for the generation 
- * and verification of one-time password using 
- * the TOTP specified algorithm.
- *
- * This class is meant to be compatible with 
- * Google Authenticator
- *
- * This class was originally ported from the rotp
- * ruby library available at https://github.com/mdp/rotp
- */
 
-use OTPHP\HOTP;
+use OTPHP\OTP;
 
 
-class TOTP extends HOTP
+class TOTP extends OTP
 {
-    /**
-     * The interval in seconds for a one-time password timeframe
-     * Defaults to 30
-     * @var integer
-     */
     protected $interval;
 
     public function __construct($secret, $interval = 30, $digest = 'sha1', $digit = 6) {
@@ -32,57 +14,26 @@ class TOTP extends HOTP
         parent::__construct($secret, $digest, $digit);
     }
 
-    /**
-     *  Get the password for a specific timestamp value 
-     *
-     *  @param integer $timestamp the timestamp which is timecoded and 
-     *  used to seed the hmac hash function.
-     *  @return integer the One Time Password
-     */
-    public function at($timestamp) {
+    public function at($timestamp)
+    {
         return $this->generateOTP($this->timecode($timestamp));
     }
 
-    /**
-     *  Get the password for the current timestamp value 
-     *
-     *  @return integer the current One Time Password
-     */
-    public function now() {
+    public function now()
+    {
         return $this->generateOTP($this->timecode(time()));
     }
 
-    /**
-     * Verify if a password is valid for a specific counter value
-     *
-     * @param integer $otp the one-time password 
-     * @param integer $timestamp the timestamp for the a given time, defaults to current time.
-     * @return  bool true if the counter is valid, false otherwise
-     */
     public function verify($otp, $timestamp = null) {
         if($timestamp === null)
             $timestamp = time();
         return ($otp === $this->at($timestamp));
     }
 
-    /**
-     * Returns the uri for a specific secret for totp method.
-     * Can be encoded as a image for simple configuration in 
-     * Google Authenticator.
-     *
-     * @param string $name the name of the account / profile
-     * @return string the uri for the hmac secret
-     */
     public function provisioningURI($name) {
         return "otpauth://totp/".urlencode($name)."?secret={$this->getSecret()}";
     }
 
-    /**
-     * Transform a timestamp in a counter based on specified internal
-     *
-     * @param integer $timestamp
-     * @return integer the timecode
-     */
     protected function timecode($timestamp) {
         return (int)( (((int)$timestamp * 1000) / ($this->getInterval() * 1000)));
     }
