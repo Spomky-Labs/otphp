f8df66b212e5f99262d603df06dc770af972f9d3
Christian Scheb <info@stylemotion.de>
2014-05-24 21:36:41 +0200

    Added test cases for session flag management

---
+++ Tests/Security/TwoFactor/SessionFlagGeneratorTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Scheb\TwoFactorBundle\Tests\Security\TwoFactor;
+
+use Scheb\TwoFactorBundle\Security\TwoFactor\SessionFlagGenerator;
+
+class SessionFlagGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \Scheb\TwoFactorBundle\Security\TwoFactor\SessionFlagGenerator
+     */
+    private $sessionFlagGenerator;
+
+    public function setUp()
+    {
+        $this->sessionFlagGenerator = new SessionFlagGenerator();
+    }
+
+    /**
+     * @test
+     */
+    public function getSessionFlag_noProviderKey_returnSessionFlag()
+    {
+        $token = $this->getMock("Symfony\Component\Security\Core\Authentication\Token\TokenInterface");
+        $token
+            ->expects($this->once())
+            ->method("getUsername")
+            ->will($this->returnValue("username"));
+
+        $returnValue = $this->sessionFlagGenerator->getSessionFlag("twoFactorProvider", $token);
+        $this->assertEquals("two_factor_twoFactorProvider_any_username", $returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function getSessionFlag_withProviderKey_returnSessionFlag()
+    {
+        $token = $this->getMockBuilder("Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken")
+            ->disableOriginalConstructor()
+            ->getMock();
+        $token
+            ->expects($this->once())
+            ->method("getUsername")
+            ->will($this->returnValue("username"));
+        $token
+            ->expects($this->once())
+            ->method("getProviderKey")
+            ->will($this->returnValue("providerKey"));
+
+        $returnValue = $this->sessionFlagGenerator->getSessionFlag("twoFactorProvider", $token);
+        $this->assertEquals("two_factor_twoFactorProvider_providerKey_username", $returnValue);
+    }
+
+}
---
+++ Tests/Security/TwoFactor/SessionFlagManagerTest.php
@@ -0,0 +1,141 @@
+<?php
+namespace Scheb\TwoFactorBundle\Tests\Security\TwoFactor;
+
+use Scheb\TwoFactorBundle\Security\TwoFactor\SessionFlagManager;
+
+class SessionFlagManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $session;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $flagGenerator;
+
+    /**
+     * @var \Scheb\TwoFactorBundle\Security\TwoFactor\SessionFlagManager
+     */
+    private $sessionFlagManager;
+
+    public function setUp()
+    {
+        $this->session = $this->getMock("Symfony\Component\HttpFoundation\Session\SessionInterface");
+        $this->flagGenerator = $this->getMock("Scheb\TwoFactorBundle\Security\TwoFactor\SessionFlagGenerator");
+        $this->sessionFlagManager = new SessionFlagManager($this->session, $this->flagGenerator);
+    }
+
+    /**
+     * @test
+     */
+    public function setBegin_startTwoFactor_flagIsSetFalse()
+    {
+        $token = $this->getMock("Symfony\Component\Security\Core\Authentication\Token\TokenInterface");
+
+        //Mock the SessionFlagGenerator
+        $this->flagGenerator
+            ->expects($this->once())
+            ->method("getSessionFlag")
+            ->with("providerName", $token)
+            ->will($this->returnValue("session_flag"));
+
+        //Mock the Session
+        $this->session
+            ->expects($this->once())
+            ->method("set")
+            ->with("session_flag", false);
+
+        $this->sessionFlagManager->setBegin("providerName", $token);
+    }
+
+    /**
+     * @test
+     */
+    public function setComplete_completeTwoFactor_flagIsSetTrue()
+    {
+        $token = $this->getMock("Symfony\Component\Security\Core\Authentication\Token\TokenInterface");
+
+        //Mock the SessionFlagGenerator
+        $this->flagGenerator
+            ->expects($this->once())
+            ->method("getSessionFlag")
+            ->with("providerName", $token)
+            ->will($this->returnValue("session_flag"));
+
+        //Mock the Session
+        $this->session
+            ->expects($this->once())
+            ->method("set")
+            ->with("session_flag", true);
+
+        $this->sessionFlagManager->setComplete("providerName", $token);
+    }
+
+    /**
+     * @test
+     */
+    public function isNotAuthenticated_notHasFlag_returnFalse()
+    {
+        $token = $this->getMock("Symfony\Component\Security\Core\Authentication\Token\TokenInterface");
+
+        //Mock the SessionFlagGenerator
+        $this->flagGenerator
+            ->expects($this->once())
+            ->method("getSessionFlag")
+            ->with("providerName", $token)
+            ->will($this->returnValue("session_flag"));
+
+        //Mock the Session
+        $this->session
+            ->expects($this->once())
+            ->method("has")
+            ->with("session_flag")
+            ->will($this->returnValue(false));
+
+        $returnValue = $this->sessionFlagManager->isNotAuthenticated("providerName", $token);
+        $this->assertFalse($returnValue);
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProvider_isNotAuthenticated
+     */
+    public function isNotAuthenticated_hasFlagSet_returnCorrectBoolean($getReturnValue, $expectedReturnValue)
+    {
+        $token = $this->getMock("Symfony\Component\Security\Core\Authentication\Token\TokenInterface");
+
+        //Mock the SessionFlagGenerator
+        $this->flagGenerator
+            ->expects($this->once())
+            ->method("getSessionFlag")
+            ->with("providerName", $token)
+            ->will($this->returnValue("session_flag"));
+
+        //Mock the Session
+        $this->session
+            ->expects($this->once())
+            ->method("has")
+            ->with("session_flag")
+            ->will($this->returnValue(true));
+        $this->session
+            ->expects($this->once())
+            ->method("get")
+            ->with("session_flag")
+            ->will($this->returnValue($getReturnValue));
+
+        $returnValue = $this->sessionFlagManager->isNotAuthenticated("providerName", $token);
+        $this->assertEquals($expectedReturnValue, $returnValue);
+    }
+
+    public function dataProvider_isNotAuthenticated()
+    {
+        return array(
+            array(true, false),
+            array(false, true),
+        );
+    }
+
+}
