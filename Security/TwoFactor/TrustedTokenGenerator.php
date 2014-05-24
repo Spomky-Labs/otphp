6ba6352263a50300ab9a5ad0a1e6a00918767194
Christian Scheb <info@stylemotion.de>
2014-05-24 21:29:16 +0200

    Made TrustedTokenGenerator better testable

--- Security/TwoFactor/TrustedTokenGenerator.php
+++ Security/TwoFactor/TrustedTokenGenerator.php
@@ -3,6 +3,7 @@ namespace Scheb\TwoFactorBundle\Security\TwoFactor;
 
 class TrustedTokenGenerator
 {
+    protected static $charspace = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     /**
      * Generate trusted computer token
@@ -13,22 +14,55 @@ class TrustedTokenGenerator
     public static function generateToken($length)
     {
         // Symfony >= 2.2: Use SecureRandom class
-        if (class_exists("Symfony\Component\Security\Core\Util\SecureRandom")) {
-            $generator = new \Symfony\Component\Security\Core\Util\SecureRandom();
-
-            return substr(base64_encode($generator->nextBytes($length)), 0, $length);
+        if (static::useSecureRandom()) {
+            return self::generateSecureToken($length);
         }
 
         // Everything else: Use less secure string generator
         else {
-            // TODO: Implement a more secure solution
-            $characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            $string = "";
-            for ($p = 0; $p < $length; $p++) {
-                $string .= $characters[mt_rand(0, strlen($characters) - 1)];
-            }
-
-            return $string;
+            return self::generateFallbackToken($length);
         }
     }
+
+    /**
+     * Check if to use Symfony's SecureRandom generator
+     *
+     * @return boolean
+     */
+    protected static function useSecureRandom()
+    {
+        return class_exists("Symfony\Component\Security\Core\Util\SecureRandom");
+    }
+
+    /**
+     * Generate a secure token with Symfony's SecureRandom generator
+     *
+     * @param integer $length
+     * @return string
+     */
+    private static function generateSecureToken($length)
+    {
+        $generator = new \Symfony\Component\Security\Core\Util\SecureRandom();
+
+        return substr(base64_encode($generator->nextBytes($length)), 0, $length);
+    }
+
+    /**
+     * Generate a random string
+     * TODO: Implement a more secure solution
+     *
+     * @param integer $length
+     * @return string
+     */
+    private static function generateFallbackToken($length)
+    {
+        $characters = static::$charspace;
+        $string = "";
+        for ($p = 0; $p < $length; $p++) {
+            $string .= $characters[mt_rand(0, strlen($characters) - 1)];
+        }
+
+        return $string;
+    }
+
 }
