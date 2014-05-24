fff99f009077258a5cad8ca599342c33595b979d
Christian Scheb <info@stylemotion.de>
2014-05-24 23:34:25 +0200

    Added test cases for compiler pass

---
+++ Tests/DependencyInjection/Compiler/ProviderCompilerPassTest.php
@@ -0,0 +1,132 @@
+<?php
+namespace Scheb\TwoFactorBundle\Tests\DependencyInjection\Compiler;
+
+use Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class ProviderCompilerPassTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass
+     */
+    private $compilerPass;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $container;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $definition;
+
+    public function setUp()
+    {
+        $this->container = $this->getMockBuilder("Symfony\Component\DependencyInjection\ContainerBuilder")
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->compilerPass = new ProviderCompilerPass();
+    }
+
+    private function stubContainerService($taggedServices)
+    {
+        $this->createServiceDefinition();
+        $this->container
+            ->expects($this->once())
+            ->method("hasDefinition")
+            ->with("scheb_two_factor.provider")
+            ->will($this->returnValue(true));
+        $this->container
+            ->expects($this->once())
+            ->method("getDefinition")
+            ->with("scheb_two_factor.provider")
+            ->will($this->returnValue($this->definition));
+        $this->container
+            ->expects($this->once())
+            ->method("findTaggedServiceIds")
+            ->with("scheb_two_factor.provider")
+            ->will($this->returnValue($taggedServices));
+    }
+
+    private function createServiceDefinition()
+    {
+        $this->definition = $this->getMockBuilder("Symfony\Component\DependencyInjection\Definition")
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @test
+     */
+    public function process_notHasDefinition_doNothing()
+    {
+        //Expect get never be called
+        $this->container
+            ->expects($this->once())
+            ->method("hasDefinition")
+            ->with("scheb_two_factor.provider")
+            ->will($this->returnValue(false));
+        $this->container
+            ->expects($this->never())
+            ->method("getDefinition");
+
+        $this->compilerPass->process($this->container);
+    }
+
+    /**
+     * @test
+     */
+    public function process_noTaggedServices_replaceArgumentWithEmptyArray()
+    {
+        $this->createServiceDefinition();
+        $taggedServices = array();
+        $this->stubContainerService($taggedServices);
+
+        //Mock the Definition
+        $this->definition
+            ->expects($this->once())
+            ->method("replaceArgument")
+            ->with(3, array());
+
+        $this->compilerPass->process($this->container);
+    }
+
+    /**
+     * @test
+     */
+    public function process_taggedServices_replaceArgumentWithServiceList()
+    {
+        $this->createServiceDefinition();
+        $taggedServices = array('serviceId' => array(
+            0 => array('alias' => 'providerAlias')
+        ));
+        $this->stubContainerService($taggedServices);
+
+        //Mock the Definition
+        $this->definition
+            ->expects($this->once())
+            ->method("replaceArgument")
+            ->with(3, array('providerAlias' => new Reference("serviceId")));
+
+        $this->compilerPass->process($this->container);
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
+     */
+    public function process_missingAlias_throwException()
+    {
+        $this->createServiceDefinition();
+        $taggedServices = array('serviceId' => array(
+            0 => array()
+        ));
+        $this->stubContainerService($taggedServices);
+
+        $this->compilerPass->process($this->container);
+    }
+}
