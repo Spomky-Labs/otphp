ddb727eb2fb98a9ea27442cd5690e40bf17a64cc
Christian Scheb <info@stylemotion.de>
2014-05-24 21:36:21 +0200

    Added test case for SchebTwoFactorBundle class

---
+++ Tests/SchebTwoFactorBundleTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Scheb\TwoFactorBundle\Tests;
+
+use Scheb\TwoFactorBundle\SchebTwoFactorBundle;
+
+class SchebTwoFactorBundleTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function build_initializeBundle_addCompilerPass()
+    {
+        $containerBuilder = $this->getMockBuilder("Symfony\Component\DependencyInjection\ContainerBuilder")
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        //Expect compiler pass to be added
+        $containerBuilder
+            ->expects($this->once())
+            ->method("addCompilerPass")
+            ->with($this->isInstanceOf("Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass"));
+
+        $bundle = new SchebTwoFactorBundle();
+        $bundle->build($containerBuilder);
+    }
+
+}
