336d16806b20937934fde106322f04f73b0fc0d1
Christian Scheb <info@stylemotion.de>
2014-07-09 21:44:48 +0200

    Added configuration option for persister

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,6 +17,7 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
+                ->scalarNode("persister")->defaultValue("scheb_two_factor.persister.doctrine")->end()
                 ->scalarNode("model_manager_name")->defaultNull()->end()
                 ->arrayNode("trusted_computer")
                     ->addDefaultsIfNotSet()
