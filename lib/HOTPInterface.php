6b326aae420358f35aae5c3b35ea2a02815ccfeb
Spomky <Spomky@users.noreply.github.com>
2014-08-11 13:36:36 +0200

    Update HOTPInterface.php

--- lib/HOTPInterface.php
+++ lib/HOTPInterface.php
@@ -6,13 +6,6 @@ interface HOTPInterface extends OTPInterface
 {
     /**
      * @return integer The initial counter (a positive integer)
-     *
-     * @deprecated since 3.0, will be removed from 4.0. Use getCounter() instead
-     */
-    public function getInitialCount();
-
-    /**
-     * @return integer The initial counter (a positive integer)
      */
     public function getCounter();
 }
