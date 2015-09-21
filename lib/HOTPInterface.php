445cb8df86ef5d919fbca4cd7d4855e280ea3bda
Florent Morselli <florent+git@morselli.fr>
2015-09-21 07:18:18 -0400

    Applied fixes from StyleCI

--- lib/HOTPInterface.php
+++ lib/HOTPInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Spomky-Labs
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace OTPHP;
 
 interface HOTPInterface extends OTPInterface
