2427adb3f27de10022e2d2b03c5500adc013dd31
Jeremy Benoist <jeremy.benoist@gmail.com>
2015-12-22 15:16:19 +0100

    Drop support for Symfony < 2.5
    Because Symfony 2.6 introduced TokenStorage which isn't supported by previous version.
    
    Also, remove support for SecureRandom and use random_bytes instead which came with Symfony 2.8. For lower version of Symfony we require `paragonie/random_compat`.
    
    Conflicts:
    	DependencyInjection/SchebTwoFactorExtension.php
    	Resources/config/persistence.xml
    	Security/TwoFactor/Trusted/TrustedTokenGenerator.php
    	Tests/Security/TwoFactor/Trusted/TrustedTokenGeneratorTest.php

--- Security/TwoFactor/Trusted/TrustedTokenGenerator.php
+++ Security/TwoFactor/Trusted/TrustedTokenGenerator.php
@@ -1,70 +1,18 @@
 <?php
+
 namespace Scheb\TwoFactorBundle\Security\TwoFactor\Trusted;
 
 class TrustedTokenGenerator
 {
-
     /**
-     * @var string
-     */
-    protected $charspace = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    /**
-     * Generate trusted computer token
+     * Generate trusted computer token.
      *
-     * @param  integer $length
+     * @param int $length
+     *
      * @return string
      */
     public function generateToken($length)
     {
-        // Symfony >= 2.2: Use SecureRandom class
-        if ($this->useSecureRandom()) {
-            return $this->generateSecureToken($length);
-        }
-
-        // Everything else: Use less secure string generator
-        else {
-            return $this->generateFallbackToken($length);
-        }
+        return substr(base64_encode(random_bytes($length)), 0, $length);
     }
-
-    /**
-     * Check if to use Symfony's SecureRandom generator
-     *
-     * @return boolean
-     */
-    protected function useSecureRandom()
-    {
-        return class_exists("Symfony\Component\Security\Core\Util\SecureRandom");
-    }
-
-    /**
-     * Generate a secure token with Symfony's SecureRandom generator
-     *
-     * @param  integer $length
-     * @return string
-     */
-    private function generateSecureToken($length)
-    {
-        $generator = new \Symfony\Component\Security\Core\Util\SecureRandom();
-
-        return substr(base64_encode($generator->nextBytes($length)), 0, $length);
-    }
-
-    /**
-     * Generate a random string
-     *
-     * @param  integer $length
-     * @return string
-     */
-    private function generateFallbackToken($length)
-    {
-        $string = "";
-        for ($p = 0; $p < $length; $p++) {
-            $string .= $this->charspace[mt_rand(0, strlen($this->charspace) - 1)];
-        }
-
-        return $string;
-    }
-
 }
--- Tests/Security/TwoFactor/Trusted/TrustedTokenGeneratorTest.php
+++ Tests/Security/TwoFactor/Trusted/TrustedTokenGeneratorTest.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Scheb\TwoFactorBundle\Tests\Security\TwoFactor\Trusted;
 
 use Scheb\TwoFactorBundle\Security\TwoFactor\Trusted\TrustedTokenGenerator;
 
 class TrustedTokenGeneratorTest extends \PHPUnit_Framework_TestCase
 {
-
     /**
      * @test
      */
@@ -15,33 +15,4 @@ class TrustedTokenGeneratorTest extends \PHPUnit_Framework_TestCase
         $token = $generator->generateToken(20);
         $this->assertEquals(20, strlen($token));
     }
-
-    /**
-     * @test
-     */
-    public function generateToken_useFallback_validToken()
-    {
-        $generator = new TestableTrustedTokenGenerator();
-        $generator->useSecureRandom = false; //Use fallback
-        $token = $generator->generateToken(20);
-        $this->assertEquals(20, strlen($token));
-        $this->assertRegExp("/^A+$/", $token);
-    }
-
-}
-
-/**
- * Makes the TrustedTokenGenerator more testable
- */
-class TestableTrustedTokenGenerator extends TrustedTokenGenerator
-{
-    public $useSecureRandom = true; //Override generator selection
-
-    protected $charspace = "A"; //Override charspace
-
-    protected function useSecureRandom()
-    {
-        return $this->useSecureRandom;
-    }
-
 }
