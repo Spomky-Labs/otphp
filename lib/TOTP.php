d3ece48e09a736c5689dc1f0fabb10c7bf5c9ae5
Florent Morselli <florent+git@morselli.fr>
2016-01-31 04:18:20 -0500

    Applied fixes from StyleCI

--- lib/TOTP.php
+++ lib/TOTP.php
@@ -3,7 +3,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2015 Spomky-Labs
+ * Copyright (c) 2014-2016 Spomky-Labs
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
