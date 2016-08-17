5cebf92407f2683e7e31d71fbeec8657a1b9942e
Saša Stamenković <umpirsky@gmail.com>
2016-08-17 17:23:08 +0200

    Add Renderer test

---
+++ Tests/Security/TwoFactor/RendererTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Scheb\TwoFactorBundle\Tests\Security\TwoFactor;
+
+use Scheb\TwoFactorBundle\Security\TwoFactor\Renderer;
+use Scheb\TwoFactorBundle\Tests\TestCase;
+
+class RendererTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function render_returnResponse()
+    {
+        $response = $this->createMock('Symfony\Component\HttpFoundation\Response');
+        $templating = $this->createMock('Symfony\Bundle\FrameworkBundle\Templating\EngineInterface');
+        $templating
+            ->expects($this->any())
+            ->method('renderResponse')
+            ->will($this->returnValue($response))
+        ;
+        $context = $this->createMock('Scheb\TwoFactorBundle\Security\TwoFactor\AuthenticationContextInterface');
+
+        $renderer = new Renderer($templating, 'AcmeTestBundle:Test:auth.html.twig');
+
+        $this->assertEquals($response, $renderer->render($context));
+    }
+}
