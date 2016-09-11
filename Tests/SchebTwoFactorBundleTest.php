3d008d59ff97131456a6fa714d99d7ca2bb1abda
Christian Scheb <info@stylemotion.de>
2016-09-11 19:49:20 +0200

    Update test

--- Tests/SchebTwoFactorBundleTest.php
+++ Tests/SchebTwoFactorBundleTest.php
@@ -2,9 +2,7 @@
 
 namespace Scheb\TwoFactorBundle\Tests;
 
-use Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass;
 use Scheb\TwoFactorBundle\SchebTwoFactorBundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SchebTwoFactorBundleTest extends TestCase
 {
@@ -13,18 +11,30 @@ class SchebTwoFactorBundleTest extends TestCase
      */
     public function build_initializeBundle_addCompilerPass()
     {
-        $containerBuilder = new ContainerBuilder();
+        $containerBuilder = $this->createMock('Symfony\Component\DependencyInjection\ContainerBuilder');
+
+        //Expect compiler pass to be added
+        $containerBuilder
+            ->expects($this->exactly(2))
+            ->method('addCompilerPass')
+            ->with($this->logicalOr(
+                $this->isInstanceOf('Scheb\TwoFactorBundle\DependencyInjection\Compiler\FirewallCompilerPass'),
+                $this->isInstanceOf('Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass')
+            ));
+
+        //Expect register authentication provider factory
+        $securityExtension = $this->createMock('Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension');
+        $containerBuilder
+            ->expects($this->once())
+            ->method('getExtension')
+            ->with('security')
+            ->willReturn($securityExtension);
+        $securityExtension
+            ->expects($this->once())
+            ->method('addSecurityListenerFactory')
+            ->with($this->isInstanceOf('\Scheb\TwoFactorBundle\DependencyInjection\Factory\Security\TwoFactorFactory'));
+
         $bundle = new SchebTwoFactorBundle();
         $bundle->build($containerBuilder);
-
-        $foundIt = false;
-        foreach ($containerBuilder->getCompilerPassConfig()->getPasses() as $pass) {
-            if ($pass instanceof ProviderCompilerPass) {
-                $foundIt = true;
-                break;
-            }
-        }
-
-        $this->assertTrue($foundIt);
     }
 }
