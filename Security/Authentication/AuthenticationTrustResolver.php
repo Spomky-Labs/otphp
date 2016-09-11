a565286155770f9c738fdc2b376642e7e30e8bee
Christian Scheb <info@stylemotion.de>
2016-09-11 14:46:14 +0200

    Use DIC decorator feature

--- Security/Authentication/AuthenticationTrustResolver.php
+++ Security/Authentication/AuthenticationTrustResolver.php
@@ -3,20 +3,50 @@ namespace Scheb\TwoFactorBundle\Security\Authentication;
 
 use Scheb\TwoFactorBundle\Security\TwoFactorToken;
 use Symfony\Component\Security\Core\Authentication\AuthenticationTrustResolver as BaseAuthenticationTrustResolver;
+use Symfony\Component\Security\Core\Authentication\AuthenticationTrustResolverInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 /**
  * Need to extend the original AuthenticationTrustResolver, because the
  * ACL security bundle depends on that class instead of using the interface.
  */
-class AuthenticationTrustResolver extends BaseAuthenticationTrustResolver
-{
+class AuthenticationTrustResolver extends BaseAuthenticationTrustResolver {
+
+    /**
+     * @var AuthenticationTrustResolverInterface
+     */
+    private $decoratedTrustResolver;
+
+    /**
+     * @param AuthenticationTrustResolverInterface $decoratedTrustResolver
+     */
+    public function __construct(AuthenticationTrustResolverInterface $decoratedTrustResolver)
+    {
+        $this->decoratedTrustResolver = $decoratedTrustResolver;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function isAnonymous(TokenInterface $token = null)
     {
-        return parent::isAnonymous($token) || $this->isTwoFactorToken($token);
+        return $this->isTwoFactorToken($token) || $this->decoratedTrustResolver->isAnonymous($token);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isRememberMe(TokenInterface $token = null)
+    {
+        return $this->decoratedTrustResolver->isRememberMe($token);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isFullFledged(TokenInterface $token = null)
+    {
+        return !$this->isTwoFactorToken($token) && $this->decoratedTrustResolver->isFullFledged();
     }
 
     /**
@@ -24,7 +54,8 @@ class AuthenticationTrustResolver extends BaseAuthenticationTrustResolver
      *
      * @return bool
      */
-    private function isTwoFactorToken(TokenInterface $token = null) {
+    private function isTwoFactorToken(TokenInterface $token = null)
+    {
         return $token instanceof TwoFactorToken;
     }
 }
