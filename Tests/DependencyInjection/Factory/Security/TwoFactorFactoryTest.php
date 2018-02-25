b1aeab275ecffa2ba6df12b975f5cf61f6b1b904
Christian Scheb <info@stylemotion.de>
2018-02-25 20:03:05 +0100

    Add test case

---
+++ Tests/DependencyInjection/Factory/Security/TwoFactorFactoryTest.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Scheb\TwoFactorBundle\Tests\DependencyInjection\Factory\Security;
+
+use Scheb\TwoFactorBundle\DependencyInjection\Factory\Security\TwoFactorFactory;
+use Scheb\TwoFactorBundle\Tests\TestCase;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Parser;
+
+class TwoFactorFactoryTest extends TestCase
+{
+    const FIREWALL_NAME = 'firewallName';
+    const CONFIG = ['config' => 'value'];
+    const USER_PROVIDER = 'userProvider';
+    const DEFAULT_ENTRY_POINT = 'defaultEntryPoint';
+    /**
+     * @var TwoFactorFactory
+     */
+    private $factory;
+
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    public function setUp()
+    {
+        $this->factory = new TwoFactorFactory();
+        $this->container = new ContainerBuilder();
+    }
+
+    private function getEmptyConfig(): array
+    {
+        $yaml = 'two_factor: ~';
+        $parser = new Parser();
+
+        return $parser->parse($yaml);
+    }
+
+    private function getFullConfig(): array
+    {
+        $yaml = <<<EOF
+two_factor:
+    check_path: /check_path
+    auth_form_path: /auth_form_path
+    always_use_default_target_path: true
+    default_target_path: /default_target_path
+EOF;
+        $parser = new Parser();
+
+        return $parser->parse($yaml);
+    }
+
+    private function processConfiguration(array $config): array
+    {
+        $firewallConfiguration = new TestableFactoryConfiguration($this->factory);
+        return (new Processor())->processConfiguration($firewallConfiguration, $config);
+    }
+
+    private function callCreateFirewall(): array
+    {
+        return $this->factory->create(
+            $this->container,
+            self::FIREWALL_NAME,
+            self::CONFIG,
+            self::USER_PROVIDER,
+            self::DEFAULT_ENTRY_POINT
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addConfiguration_emptyConfig_setDefaultValues()
+    {
+        $config = $this->getEmptyConfig();
+        $processedConfiguration = $this->processConfiguration($config);
+
+        $this->assertEquals('/2fa_check', $processedConfiguration['check_path']);
+        $this->assertEquals('/2fa', $processedConfiguration['auth_form_path']);
+        $this->assertEquals(false, $processedConfiguration['always_use_default_target_path']);
+        $this->assertEquals('/', $processedConfiguration['default_target_path']);
+    }
+
+    /**
+     * @test
+     */
+    public function addConfiguration_fullConfig_setConfigValues()
+    {
+        $config = $this->getFullConfig();
+        $processedConfiguration = $this->processConfiguration($config);
+
+        $this->assertEquals('/check_path', $processedConfiguration['check_path']);
+        $this->assertEquals('/auth_form_path', $processedConfiguration['auth_form_path']);
+        $this->assertEquals(true, $processedConfiguration['always_use_default_target_path']);
+        $this->assertEquals('/default_target_path', $processedConfiguration['default_target_path']);
+    }
+
+    /**
+     * @test
+     */
+    public function create_returnServiceIds()
+    {
+        $returnValue = $this->callCreateFirewall();
+
+        $this->assertEquals('security.authentication.provider.two_factor.firewallName', $returnValue[0]);
+        $this->assertEquals('security.authentication.listener.two_factor.firewallName', $returnValue[1]);
+        $this->assertEquals(self::DEFAULT_ENTRY_POINT, $returnValue[2]);
+    }
+
+    /**
+     * @test
+     */
+    public function create_createAuthenticationProvider()
+    {
+        $this->callCreateFirewall();
+
+        $this->assertTrue($this->container->hasDefinition('security.authentication.provider.two_factor.firewallName'));
+        $definition = $this->container->getDefinition('security.authentication.provider.two_factor.firewallName');
+        $this->assertEquals(self::FIREWALL_NAME, $definition->getArgument(1));
+    }
+
+    /**
+     * @test
+     */
+    public function create_createAuthenticationListener()
+    {
+        $this->callCreateFirewall();
+
+        $this->assertTrue($this->container->hasDefinition('security.authentication.listener.two_factor.firewallName'));
+        $definition = $this->container->getDefinition('security.authentication.listener.two_factor.firewallName');
+        $this->assertEquals(self::FIREWALL_NAME, $definition->getArgument(3));
+        $this->assertEquals('security.authentication.success_handler.two_factor.firewallName', (string) $definition->getArgument(4));
+        $this->assertEquals('security.authentication.failure_handler.two_factor.firewallName', (string) $definition->getArgument(5));
+        $this->assertEquals(self::CONFIG, $definition->getArgument(6));
+    }
+
+    /**
+     * @test
+     */
+    public function create_createSuccessHandler()
+    {
+        $this->callCreateFirewall();
+
+        $this->assertTrue($this->container->hasDefinition('security.authentication.success_handler.two_factor.firewallName'));
+        $definition = $this->container->getDefinition('security.authentication.success_handler.two_factor.firewallName');
+        $this->assertEquals(self::FIREWALL_NAME, $definition->getArgument(1));
+        $this->assertEquals(self::CONFIG, $definition->getArgument(2));
+    }
+
+    /**
+     * @test
+     */
+    public function create_createFailureHandler()
+    {
+        $this->callCreateFirewall();
+
+        $this->assertTrue($this->container->hasDefinition('security.authentication.failure_handler.two_factor.firewallName'));
+        $definition = $this->container->getDefinition('security.authentication.failure_handler.two_factor.firewallName');
+        $this->assertEquals(self::CONFIG, $definition->getArgument(1));
+    }
+}
+
+// Helper class to process config
+class TestableFactoryConfiguration implements ConfigurationInterface
+{
+    /**
+     * @var TwoFactorFactory
+     */
+    private $factory;
+
+    public function __construct(TwoFactorFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root(TwoFactorFactory::AUTHENTICATION_PROVIDER_KEY);
+        $this->factory->addConfiguration($rootNode);
+        return $treeBuilder;
+    }
+}
