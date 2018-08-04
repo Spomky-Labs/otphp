92e8335e8815febbf2327baa28b251f072e332e3
peter279k <peter279k@gmail.com>
2018-08-05 00:50:56 +0800

    Test enhancement

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace OTPHP\Test;
 
 use OTPHP\Factory;
+use OTPHP\HOTP;
+use OTPHP\TOTP;
 use PHPUnit\Framework\TestCase;
 
 final class FactoryTest extends TestCase
@@ -23,7 +25,7 @@ final class FactoryTest extends TestCase
         $otp = 'otpauth://totp/My%20Project%3Aalice%40foo.bar?algorithm=sha512&digits=8&foo=bar.baz&issuer=My%20Project&period=20&secret=JDDK4U6G3BJLEZ7Y';
         $result = Factory::loadFromProvisioningUri($otp);
 
-        $this->assertInstanceOf('\OTPHP\TOTP', $result);
+        $this->assertInstanceOf(TOTP::class, $result);
         $this->assertEquals('My Project', $result->getIssuer());
         $this->assertEquals('alice@foo.bar', $result->getLabel());
         $this->assertEquals('sha512', $result->getDigest());
@@ -53,7 +55,7 @@ final class FactoryTest extends TestCase
         $otp = 'otpauth://hotp/My%20Project%3Aalice%40foo.bar?counter=1000&digits=8&image=https%3A%2F%2Ffoo.bar%2Fbaz&issuer=My%20Project&secret=JDDK4U6G3BJLEZ7Y';
         $result = Factory::loadFromProvisioningUri($otp);
 
-        $this->assertInstanceOf('\OTPHP\HOTP', $result);
+        $this->assertInstanceOf(HOTP::class, $result);
         $this->assertEquals('My Project', $result->getIssuer());
         $this->assertEquals('alice@foo.bar', $result->getLabel());
         $this->assertEquals('sha1', $result->getDigest());
@@ -130,7 +132,7 @@ final class FactoryTest extends TestCase
         $otp = 'otpauth://totp/My%20Test%20-%20Auth?secret=JDDK4U6G3BJLEZ7Y';
         $result = Factory::loadFromProvisioningUri($otp);
 
-        $this->assertInstanceOf('\OTPHP\TOTP', $result);
+        $this->assertInstanceOf(TOTP::class, $result);
         $this->assertNull($result->getIssuer());
         $this->assertEquals('My Test - Auth', $result->getLabel());
         $this->assertEquals('sha1', $result->getDigest());
